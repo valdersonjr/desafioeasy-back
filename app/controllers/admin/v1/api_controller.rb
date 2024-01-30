57c79b68395fc1a30f18cb0cc3fdfb95e2642460
HygorMeireles <hygorgabriel23@hotmail.com>
2024-01-30 11:07:04 -0300

    fixing some bugs

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -2,7 +2,7 @@ module Admin::V1
     class ApiController < ApplicationController
         private
 
-        def render_error(fields: nil, message: 'Não foi possivel criar, os parâmetros são inválidos!', status: :unprocessable_entity)
+        def render_error(fields: nil, message: 'Não foi possível criar, os parâmetros são inválidos ou o objeto já existe!', status: :unprocessable_entity)
           render json: { message: message, fields: fields }, status: status
         end
       end
