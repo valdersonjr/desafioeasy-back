94af6f1cf2b7ab1b524da9358317d5c1dbcf2837
HygorMeireles <hygorgabriel23@hotmail.com>
2024-04-17 11:22:58 -0300

    Correction in order_product_spec

--- spec/models/order_product_spec.rb
+++ spec/models/order_product_spec.rb
@@ -8,5 +8,5 @@ RSpec.describe OrderProduct, type: :model do
     it { is_expected.to validate_presence_of(:quantity) }
 
     subject { FactoryBot.create(:order_product) }
-    it { is_expected.to validate_uniqueness_of(:product_id).scoped_to(:order_id) }
+    it { is_expected.to validate_presence_of(:product_id) }
 end
