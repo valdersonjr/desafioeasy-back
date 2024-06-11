f7a28fffedb82f66b5769c0de9fedc8aac2398bd
valdersonjr <valdersonpontes@gmail.com>
2024-06-11 14:07:50 -0300

    6d

--- app/models/concerns/paginatable.rb
+++ app/models/concerns/paginatable.rb
@@ -7,11 +7,11 @@ module Paginatable
   DEFAULT_PAGE = 1                                                    # Constante que define o número da página padrão, caso não seja especificada.
 
   included do                                                         # O bloco included é executado no contexto da classe que inclui o módulo.
-    scope :paginate, -> (page, length) do                             # Define um escopo chamado :paginate que aceita dois parâmetros: page e length.
+    scope :custom_paginate, -> (page, length) do                      # Define um escopo chamado :custom_paginate que aceita dois parâmetros: page e length.
       page = page.present? && page > 0 ? page : DEFAULT_PAGE          # Determina a página atual. Se 'page' não estiver presente ou for menor que 1, usa DEFAULT_PAGE.
       length = length.present? && length > 0 ? length : MAX_PER_PAGE  # Determina o comprimento (número de registros por página). Se 'length' não estiver presente ou for menor que 1, usa MAX_PER_PAGE.
       starts_at = (page - 1) * length                                 # Calcula o índice do primeiro registro na página, com base na página atual e no comprimento.
       limit(length).offset(starts_at)                                 # Usa os métodos 'limit' e 'offset' do ActiveRecord para limitar o número de registros retornados e pular um certo número de registros, respectivamente, efetivamente paginando os resultados.
     end
   end
-end 
+end
